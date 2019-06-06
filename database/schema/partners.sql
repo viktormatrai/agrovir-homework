1c8a0fcb76efbda9f35240edd798c733864c2b50
Viktor Matrai <viktor0matrai@gmail.com>
2019-06-06 07:38:10 +0200

    schema update

--- database/schema/partners.sql
+++ database/schema/partners.sql
@@ -20,11 +20,12 @@ create table if not exists partners
 );
 
 ALTER TABLE partners
-insert into agrovir_homework.partners (company_name, tax_number, company_registration_number, address, bank_account_number, phone_number, company_type_id, location_id, note) values ('Budapest company', 0000000000, 0000000000000, '1011 Clarck Adam sqr 1.', 00000000, +36111111111, 1, 1, 'ok');
+  insert into agrovir_homework.partners (company_name, tax_number, company_registration_number, address, bank_account_number, phone_number, company_type_id, location_id, note)
+    values ('Budapest company', '0000000000', '0000000000000', '1011 Clarck Adam sqr 1.', '00000000', '+36111111111', '1', '1', 'ok');
 ALTER TABLE partners
-insert into agrovir_homework.partners (company_name, tax_number, company_registration_number, address, bank_account_number, phone_number, company_type_id, location_id, note) values ('Kaposvar company', 0000000001, 0000000000001, '1011 Budapesti str 1.', 00000001, +36851111111, 2, 2, null);
+  insert into agrovir_homework.partners (company_name, tax_number, company_registration_number, address, bank_account_number, phone_number, company_type_id, location_id, note)
+    values ('Kaposvar company', '0000000001', '0000000000001', '1011 Budapesti str 1.', '00000001', '+36851111111', '2', '2', null);
 ALTER TABLE partners
-insert into agrovir_homework.partners (company_name, tax_number, company_registration_number, address, bank_account_number, phone_number, company_type_id, location_id, note) values ('Kaposvar company', 0000000011, 0000000000002, '1011 Petőfi S. str 1.', 00000001, +36701111111, 3, 3, Keszthelyi telephely);
-
-
+  insert into agrovir_homework.partners (company_name, tax_number, company_registration_number, address, bank_account_number, phone_number, company_type_id, location_id, note)
+    values ('Kaposvar company', '0000000011', '0000000000002', '1011 Petőfi S. str 1.', '00000001', '+36701111111', '3', '3', 'Keszthelyi telephely');
 
